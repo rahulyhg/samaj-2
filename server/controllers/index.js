29be1b28658197c0705d6bb2b401081c13cf06d3
Asif Noor <asif@azrisolutions.com>
2014-05-15 11:57:22 +0200

    Added controllers and views for issues, ideas and suggestions

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,27 +1,67 @@
 'use strict';
 
-var mean = require('meanio');
-
+var request = require('request');
+var async = require('async');
+var issues_output = '';
+var latest_issues_output = '';
+var banner_output = '';
+var suggestions_output ='';
+var ideas_output = '';
 exports.render = function(req, res) {
-    var modules = [];
-    
-    // Preparing angular modules list with dependencies
-    for (var name in mean.modules) {
-        modules.push({
-            name: name,
-            module: 'mean.' + name,
-            angularDependencies: mean.modules[name].angularDependencies
-        });
-    }
+    var url = 'http://10.11.1.29:8082/issues/node';
+    var latest_issues_url = 'http://10.11.1.29:8082/latest-issues';
+    var banner_images_url = 'http://10.11.1.29:8082/banner-images';
+    var ideas_url = 'http://10.11.1.29:8082/ideas';
+    var suggestions_url = 'http://10.11.1.29:8082/suggestions';
 
-    // Send some basic starting info to the view
-    res.render('index', {
-        user: req.user ? JSON.stringify({
-            name: req.user.name,
-            _id: req.user._id,
-            username: req.user.username,
-            roles: (req.user ? req.user.roles : ['anonymous'])
-        }) : 'null',
-        modules: JSON.stringify(modules)
+    async.parallel({
+	get_issues : function(callback) {
+	    request(url, function(error, response, html) {
+		if(!error) {
+		    issues_output = JSON.parse(response.body);
+		}
+		callback(null, issues_output);
+	    });
+	},
+	latest_issues : function(callback) {
+	    request(latest_issues_url, function(error, response, html) {
+		if(!error) {
+		    latest_issues_output = JSON.parse(response.body);
+		}
+		callback(null, latest_issues_output);
+	    });
+	},
+	banner_images : function(callback) {
+	    request(banner_images_url, function(error, response, html) {
+		if(!error) {
+		    banner_output = JSON.parse(response.body);
+		}
+		callback(null, banner_output);
+	    });
+	},
+	ideas : function(callback) {
+	    request(ideas_url, function(error, response, html) {
+		if(!error) {
+		    ideas_output = JSON.parse(response.body);
+		}
+		callback(null, ideas_output);
+	    });
+	},
+	suggestions : function(callback) {
+	    request(suggestions_url, function(error, response, html) {
+		if(!error) {
+		    suggestions_output = JSON.parse(response.body);
+		}
+		callback(null, suggestions_output);
+	    });
+	}
+    }, function (err,data){
+	res.render('index', {
+	    issues: data.get_issues,
+	    latest_issues: data.latest_issues,
+	    banner_images: data.banner_images,
+	    ideas: data.ideas,
+	    suggestions: data.suggestions
+	});
     });
 };
--- server/controllers/issues.js
+++ server/controllers/issues.js
@@ -1,8 +1,9 @@
 'use strict';
 
 
-
+//var request = require('request');
 exports.list = function(req, res) {
+
     // Send some basic starting info to the view
     res.render('issues', {username: 'hello world'});
 };
