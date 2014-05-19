a84628685e3693390b89affd728bc8b9c1f38f40
root <root@asif.vpn.local>
2014-05-19 15:38:17 +0200

    Added code to handle individual issue

--- server/controllers/issues.js
+++ server/controllers/issues.js
@@ -1,9 +1,28 @@
 'use strict';
+var issue_detail = '';
+var request = require('request');
+var async = require('async');
 
+/**
+ * Show an article
+ */
 
-//var request = require('request');
-exports.list = function(req, res) {
-
-    // Send some basic starting info to the view
-    res.render('issues', {username: 'hello world'});
+exports.show = function(req, res) {
+    var issue_url = 'http://10.11.1.29:8082/issues/node/'+req.params.issueId;
+//    var issue_url = 'http://10.11.1.29:8082/issues/node/12';
+    async.parallel({
+	issue : function(callback) {
+	    request(issue_url, function(error, response, html) {
+		if(!error) {
+		    issue_detail = JSON.parse(response.body);
+		}
+		callback(null, issue_detail);
+	    });
+	}
+    }, function (err,data){
+	res.render('issues', {
+	    issues: data.issue
+	});
+    });
 };
+
