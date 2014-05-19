2fb4b6f36a1c0a1fead4137bc5613dc9a9a0a77b
root <root@asif.vpn.local>
2014-05-19 15:40:29 +0200

    Added code to handle individual issue

--- server/routes/issues.js
+++ server/routes/issues.js
@@ -1,13 +1,8 @@
 'use strict';
-
 module.exports = function(app) {
 
     // Home route
     var issues = require('../controllers/issues');
-    //app.route('/issues').get(index.list);
-
-    app.get('/issues', issues.list);
-    
-
+    app.get('/issues/:issueId', issues.show);
 };
 
